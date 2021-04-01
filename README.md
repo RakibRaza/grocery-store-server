b69085f8cb6e04f4a0f3ffc09d7ce427f8598233
Rakib Raza <rakibraza159@gmail.com>
2021-04-01 19:58:21 +0600

    readme.md file edit

---
+++ README.md
@@ -0,0 +1,52 @@
+## Website Name
+
+### `Grocery Store`
+
+## Website Link
+
+(Grocery Store)[https://knight-riders-bd.web.app/]
+
+## Website Feature
+
+#### `Firebase Authentication`
+
+#### `SignIn Method`
+
+1. Email & Password
+2. Google
+3. Facebook
+4. Github
+
+#### `Backend Tecnology`
+
+1. NodeJS
+2. ExpressJS
+3. MongoDB
+4. Heroku
+
+#### `Home Page`
+
+1. Search Input
+2. Product Card
+
+#### `Checkout Page`
+
+1. Checkout Product Details
+
+#### `Orders Page`
+
+1. Order Summary
+
+#### `Login Page`
+
+1. Login Form
+
+#### `Signup Page`
+
+1. Signup Form
+
+#### `Admin Page`
+
+1. Manage Product (Manage your all product with delete and edit option)
+2. Add Product (A form with four input field(Product Name,Add Price,Wight, Add Photo). where you can Add Your own product)
+3. Edit Product
--- index.js
+++ index.js
@@ -1,13 +1,70 @@
 const express = require("express");
 const cors = require("cors");
-const app = express();
+const MongoClient = require("mongodb").MongoClient;
+const ObjectId = require("mongodb").ObjectId;
 require("dotenv").config();
+
+const app = express();
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jljnw.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+client.connect((err) => {
+  const productCollection = client
+    .db(process.env.DB_NAME)
+    .collection("products");
+  const orderCollection = client.db(process.env.DB_NAME).collection("orders");
+  // Get Products
+  app.get("/products", (req, res) => {
+    productCollection
+      .find({})
+      .toArray((err, collection) => res.send(collection));
+  });
+  // Get single Products
+  app.get("/product/:id", (req, res) => {
+    productCollection
+      .find({ _id: ObjectId(req.params.id) })
+      .toArray((err, collection) => res.send(collection));
+  });
+
+  // Add Product
+  app.post("/addProduct", (req, res) => {
+    const product = req.body;
+    productCollection
+      .insertOne(product)
+      .then((result) => res.send(result.insertedCount > 0));
+  });
+
+  // Place Orders
+  app.post("/placeOrders", (req, res) => {
+    const orders = req.body;
+    orderCollection
+      .insertOne(orders)
+      .then((result) => res.send(result.insertedCount > 0));
+  });
+  // Get Orders By Email
+  app.get("/orders", (req, res) => {
+    orderCollection
+      .find({ email: req.query.email })
+      .toArray((err, collection) => {
+        res.send(collection);
+      });
+  });
+  // Delete product from database
+  app.delete("/deletePproduct/:id", (req, res) => {
+    productCollection
+      .deleteOne({ _id: ObjectId(req.params.id) })
+      .then((result) => res.send(result.deletedCount > 0));
+  });
 });
 
 const port = process.env.PORT || 8000;
